d69796cca3e5a5219f132511e0e82045cd2f39f3
meg768 <meg768@users.noreply.github.com>
2023-04-14 02:57:46 +0200

    -

--- mqtt-async.js
+++ mqtt-async.js
@@ -1,56 +1,56 @@
 module.exports = function MqttAsync(client) {
 
-	let subscribe = client.subscribe;
-	let publish = client.publish;
-	let unsubscribe = client.unsubscribe;
-	let end = client.end;
+    let subscribe = client.subscribe;
+    let publish = client.publish;
+    let unsubscribe = client.unsubscribe;
+    let end = client.end;
 
-	client.publish = (...args) => {
-		return new Promise((resolve, reject) => {
-			publish.call(client, ...args, (error) => {
-				if (error)
-					reject(error);
-				else
-					resolve();
-			});
-		});
-	}
+    client.publish = (...args) => {
+        return new Promise((resolve, reject) => {
+            publish.call(client, ...args, (error) => {
+                if (error)
+                    reject(error);
+                else
+                    resolve();
+            });
+        });
+    }
 
-	client.subscribe = (...args) => {
-		return new Promise((resolve, reject) => {
-			subscribe.call(client, ...args, (error) => {
-				if (error)
-					reject(error);
-				else
-					resolve();
-			});
-		});
+    client.subscribe = (...args) => {
+        return new Promise((resolve, reject) => {
+            subscribe.call(client, ...args, (error) => {
+                if (error)
+                    reject(error);
+                else
+                    resolve();
+            });
+        });
 
-	}
+    }
 
-	client.unsubscribe = (...args) => {
-		return new Promise((resolve, reject) => {
-			unsubscribe.call(client, ...args, (error) => {
-				if (error)
-					reject(error);
-				else
-					resolve();
-			});
-		});
+    client.unsubscribe = (...args) => {
+        return new Promise((resolve, reject) => {
+            unsubscribe.call(client, ...args, (error) => {
+                if (error)
+                    reject(error);
+                else
+                    resolve();
+            });
+        });
 
-	}
+    }
 
-	client.end = (...args) => {
-		return new Promise((resolve, reject) => {
-			end.call(client, ...args, (error) => {
-				if (error)
-					reject(error);
-				else
-					resolve();
-			});
-		});
+    client.end = (...args) => {
+        return new Promise((resolve, reject) => {
+            end.call(client, ...args, (error) => {
+                if (error)
+                    reject(error);
+                else
+                    resolve();
+            });
+        });
 
-	}
+    }
 
-	return client;
+    return client;
 }
