b4af06040aa76f0d773c55118ecf673dd392c725
meg768 <meg768@users.noreply.github.com>
2023-04-14 02:56:47 +0200

    -

--- mqtt-async.js
+++ mqtt-async.js
@@ -1,56 +1,56 @@
 module.exports = function MqttAsync(client) {
-    
-    let subscribe = client.subscribe;
-    let publish = client.publish;
-    let unsubscribe = client.unsubscribe;
-    let end = client.end;
+
+	let subscribe = client.subscribe;
+	let publish = client.publish;
+	let unsubscribe = client.unsubscribe;
+	let end = client.end;
 
 	client.publish = (...args) => {
 		return new Promise((resolve, reject) => {
 			publish.call(client, ...args, (error) => {
 				if (error)
 					reject(error);
-				else 
+				else
 					resolve();
-			});	
+			});
 		});
-	}  
+	}
 
-    client.subscribe = (...args) => {
+	client.subscribe = (...args) => {
 		return new Promise((resolve, reject) => {
 			subscribe.call(client, ...args, (error) => {
 				if (error)
 					reject(error);
-				else 
+				else
 					resolve();
 			});
 		});
 
 	}
 
-    client.unsubscribe = (...args) => {
+	client.unsubscribe = (...args) => {
 		return new Promise((resolve, reject) => {
 			unsubscribe.call(client, ...args, (error) => {
 				if (error)
 					reject(error);
-				else 
+				else
 					resolve();
 			});
 		});
 
 	}
 
-    client.end = (...args) => {
+	client.end = (...args) => {
 		return new Promise((resolve, reject) => {
 			end.call(client, ...args, (error) => {
 				if (error)
 					reject(error);
-				else 
+				else
 					resolve();
 			});
 		});
 
 	}
 
-    return client;
+	return client;
 }
